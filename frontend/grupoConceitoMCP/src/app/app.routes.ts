6c062cdae6a7e82ca51d2236f48f77bb7623bf9f
danifc123 <danielcarmo29@gmail.com>
2026-07-09 23:00:17 -0300

    fix: configurando rotas

--- frontend/grupoConceitoMCP/src/app/app.routes.ts
+++ frontend/grupoConceitoMCP/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Transacoes } from './pages/financeiro/transacoes/transacoes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'financeiro/transacoes', component: Transacoes },
+  { path: '**', redirectTo: '' }
+];

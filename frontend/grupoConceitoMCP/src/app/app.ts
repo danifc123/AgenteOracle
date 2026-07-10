ebcceb0c461fdcec4c855f539812a2e255afec88
danifc123 <danielcarmo29@gmail.com>
2026-07-09 23:00:48 -0300

    fix: ajustando app e arquivo style com variaveis

--- frontend/grupoConceitoMCP/src/app/app.ts
+++ frontend/grupoConceitoMCP/src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('grupoConceitoMCP');
+  protected readonly title = signal('Grupo Conceito');
+  protected readonly sidebarOpen = signal(false);
+  protected readonly financeiroOpen = signal(true);
+
+  toggleSidebar(): void {
+    this.sidebarOpen.update((value) => !value);
+  }
+
+  closeSidebar(): void {
+    this.sidebarOpen.set(false);
+  }
+
+  toggleFinanceiro(): void {
+    this.financeiroOpen.update((value) => !value);
+  }
 }

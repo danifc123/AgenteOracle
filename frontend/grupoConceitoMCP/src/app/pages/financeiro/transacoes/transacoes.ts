b663f15e5837e5e8dc80c1ea0c94168986973357
danifc123 <danielcarmo29@gmail.com>
2026-07-09 23:00:02 -0300

    feat: adicionando pagina de transações

---
+++ frontend/grupoConceitoMCP/src/app/pages/financeiro/transacoes/transacoes.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transacoes',
+  imports: [],
+  templateUrl: './transacoes.html',
+  styleUrl: './transacoes.scss'
+})
+export class Transacoes {}

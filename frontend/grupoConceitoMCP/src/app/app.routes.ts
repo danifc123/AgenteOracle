896a9455fe22f789fd9f0ff0b31fda640f9ef780
danifc123 <danielcarmo29@gmail.com>
2026-07-14 10:22:29 -0300

    adicionando fontawssome para icones

--- frontend/grupoConceitoMCP/src/app/app.routes.ts
+++ frontend/grupoConceitoMCP/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
-import { Transacoes } from './pages/financeiro/transacoes/transacoes';
+import { ModuloFinanceiro } from './pages/financeiro/modulo/modulo';
 import { Chat } from './pages/chat/chat';
 import { Historico } from './pages/relatorios/historico/historico';
 
 export const routes: Routes = [
   { path: '', component: Home },
-  { path: 'financeiro/transacoes', component: Transacoes },
+  { path: 'financeiro/:moduloId', component: ModuloFinanceiro },
   { path: 'chat', component: Chat },
   { path: 'relatorios/historico', component: Historico },
   { path: '**', redirectTo: '' }
